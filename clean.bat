e000830ac0bc9341338ca03ca2cee882df0002ff
songjj333 <50099339+S33J33J33@users.noreply.github.com>
2024-01-23 22:44:58 +0800

    Add files via upload

---
+++ clean.bat
@@ -0,0 +1,41 @@
+del *.aux /S
+del *.bak /S
+del *.sav /S
+del *.log /S
+
+del *.bbl
+
+del *.dvi
+
+del *.blg
+
+del *.thm
+del *.toc
+
+del *.toe
+
+
+del *.lof 
+
+del *.lot
+
+del *.out 
+
+
+del *.fen
+
+del *.ten
+
+del *.ps
+
+
+del *.gz
+
+del *.gz(busy)
+
+del *.synctex
+
+del *.synctex(busy)
+
+
+del *.loa
